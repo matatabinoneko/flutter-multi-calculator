afc2a8675c3fcb3672a06950ceb7a23dd02fe93f
matatabinoneko <matatabinoneko0721@gmail.com>
2022-09-03 22:29:05 +0900

    fix #27

---
+++ app/lib/global/provider/shared_preference_provider.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sharedPrefProvider = FutureProvider<SharedPreferences>((ref) async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs;
+});
